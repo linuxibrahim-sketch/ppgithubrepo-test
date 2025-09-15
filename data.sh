8d623523ce8ff9796a897f28de27b278ddd0835b
linuxibrahim-sketch <linux.ibrahim@gmail.com>
2025-09-15 09:03:42 +0530

    added data file

---
+++ data.sh
@@ -0,0 +1,41 @@
+# comparison operator
+# a = 13
+# b = 33
+# print(a > b)
+# print(a < b)
+# print(a == b)
+# print(a != b)
+# print(a >= b)
+# print(a <= b)
+
+#logical operator
+
+# a= True
+# b= True
+# print(a and b)
+# print(a or b)
+# print(not b)
+
+#bitwise operators
+
+
+a = 10
+b = 4
+print(a & b)
+print(a | b)
+print(~a)
+print(a ^ b)
+
+a="10"
+b=15
+
+print(type(a))
+print(type(b))
+print("a+b= is addition: ",a+b)
+
+print("a-b= ",    a-b)
+print("a*b= ",a*b)
+print("a/b= ",a/b)
+print("a//b= ",a//b)
+print(" modulus is ",a%b)
+
